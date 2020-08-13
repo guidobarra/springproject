203d8c397136892c0e8e47765d1689e3a509388d
ITRESOURCES\Guido_Barra <guido_barra@itrsa.com.ar>
2020-08-12 22:59:04 -0300

    login custom page

--- spring-security/src/main/java/com/guba/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/guba/springsecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.guba.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,5 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .withUser(userBuilder.username("john").password("test123").roles("EMPLOYEE"))
             .withUser(userBuilder.username("mary").password("test123").roles("MANAGER"))
             .withUser(userBuilder.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
     }
 }
