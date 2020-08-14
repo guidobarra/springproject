f673cdb1c125a1c797df3419ea0b38fc364da348
ITRESOURCES\Guido_Barra <guido_barra@itrsa.com.ar>
2020-08-14 20:08:42 -0300

    config database, mysql and spring security

---
+++ spring-security/sql-scripts/setup-spring-security-demo-database-plaintext.sql
@@ -0,0 +1,58 @@
+DROP DATABASE  IF EXISTS `db_spring_security`;
+
+CREATE DATABASE  IF NOT EXISTS `db_spring_security`;
+USE `db_spring_security`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+('john','{noop}test123',1),
+('mary','{noop}test123',1),
+('susan','{noop}test123',1);
+
+-- {noop} The encoding algorithm id
+-- Let's Spring Security know the passwords are
+-- stored as plain text (noop)
+
+
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('john','ROLE_EMPLOYEE'),
+('mary','ROLE_EMPLOYEE'),
+('mary','ROLE_MANAGER'),
+('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_ADMIN');
+
+
--- spring-security/src/main/java/com/guba/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/guba/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.guba.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +8,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // Inject our data source that we just configured in "DemoAppConfig"
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // spring security used table user and authorities for roles, password, read register
+        // No Longer hard-coding users :-)
+        // Tell spring security to use JDBC authentication with our data source
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+
+        /**
         User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
         auth.inMemoryAuthentication()
             .withUser(userBuilder.username("john").password("test123").roles("EMPLOYEE"))
             .withUser(userBuilder.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
             .withUser(userBuilder.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+         */
     }
 
     @Override
