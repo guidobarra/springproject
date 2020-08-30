4b29cc6dacc7b65113cc7cc8f0630130e09673d1
ITRESOURCES\Guido_Barra <guido_barra@itrsa.com.ar>
2020-08-30 18:57:37 -0300

    Security configuration and BASIC AUTH

--- spring-boot-security/src/main/java/com/guba/demo/DemoApplication.java
+++ spring-boot-security/src/main/java/com/guba/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.guba.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-security/src/main/java/com/guba/demo/student/Student.java
+++ spring-boot-security/src/main/java/com/guba/demo/student/Student.java
@@ -1,4 +1,4 @@
-package com.example.demo.student;
+package com.guba.demo.student;
 
 public class Student {
 
