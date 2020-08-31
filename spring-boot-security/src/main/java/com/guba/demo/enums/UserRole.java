815e56951fcf9fec0bc521f919df1a8c491a5210
ITRESOURCES\Guido_Barra <guido_barra@itrsa.com.ar>
2020-08-30 23:27:20 -0300

    PERMISSION BASED AND AUTHENTICATION IN ACTION

--- spring-boot-security/src/main/java/com/guba/demo/enums/UserRole.java
+++ spring-boot-security/src/main/java/com/guba/demo/enums/UserRole.java
@@ -1,10 +1,12 @@
 package com.guba.demo.enums;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import static  com.guba.demo.enums.UserPermission.*;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum UserRole {
     STUDENT(Sets.newHashSet()),
@@ -15,5 +17,22 @@ public enum UserRole {
 
     UserRole(Set<UserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<UserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthority() {
+
+        // add all permission
+        Set<SimpleGrantedAuthority> rolePermissions = permissions
+                                                .stream()
+                                                .map(p -> new SimpleGrantedAuthority(p.getPermission()))
+                                                .collect(Collectors.toSet());
+
+        // add role
+        rolePermissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return rolePermissions;
     }
 }
--- spring-boot-security/src/main/java/com/guba/demo/security/ApplicationSecurityConfig.java
+++ spring-boot-security/src/main/java/com/guba/demo/security/ApplicationSecurityConfig.java
@@ -3,15 +3,20 @@ package com.guba.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.Set;
+
+import static com.guba.demo.enums.UserPermission.COURSE_WRITE;
 import static com.guba.demo.enums.UserRole.*;
 
 @Configuration
@@ -33,6 +38,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasAnyRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINING.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,11 +52,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected UserDetailsService userDetailsService() {
         // simulate data base
-        UserDetails guidinhoUser = createUserDetail("guidinho", "password", STUDENT.name());
+        /**
+        UserDetails guidinhoUser = createUserDetailRole("guidinho", "password", STUDENT.name());
 
-        UserDetails luciaUser = createUserDetail("lucia", "password123", ADMIN.name());
+        UserDetails luciaUser = createUserDetailRole("lucia", "password123", ADMIN.name());
 
-        UserDetails tomUser = createUserDetail("tom", "password123", ADMINTRAINING.name());
+        UserDetails tomUser = createUserDetailRole("tom", "password123", ADMINTRAINING.name());
+        */
+        UserDetails guidinhoUser = createUserDetailRoleAndPermissions("guidinho", "password", STUDENT.getGrantedAuthority());
+
+        UserDetails luciaUser = createUserDetailRoleAndPermissions("lucia", "password123", ADMIN.getGrantedAuthority());
+
+        UserDetails tomUser = createUserDetailRoleAndPermissions("tom", "password123", ADMINTRAINING.getGrantedAuthority());
 
         return new InMemoryUserDetailsManager(
                 guidinhoUser,
@@ -55,11 +71,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 tomUser);
     }
 
-    private UserDetails createUserDetail(String userName, String pass, String role) {
+    private UserDetails createUserDetailRole(String userName, String pass, String role) {
         return User.builder()
                 .username(userName)
                 .password(passwordEncoder.encode(pass))
                 .roles(role) // is ROLE_STUDENT or ROLE_ADMIN etc
+                .build();
+    }
+
+    private UserDetails createUserDetailRoleAndPermissions(String userName, String pass, Set<SimpleGrantedAuthority> roleAndPermissions) {
+        return User.builder()
+                .username(userName)
+                .password(passwordEncoder.encode(pass))
+                .authorities(roleAndPermissions) // ROLE_STUDENT and permissions of ROLE_STUDENT etc
                 .build();
     }
 }
